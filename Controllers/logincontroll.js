3f72d743acfbf6c6836105a4d2075a143024fc8d
hayaat <khadrabashiir4323@gmail.com>
2023-08-12 20:42:10 +0300

    tokenchange

--- Controllers/logincontroll.js
+++ Controllers/logincontroll.js
@@ -14,7 +14,7 @@ const jwt=require('jsonwebtoken')
     const basscheck=await bycrypt.compare(req.body.password,user.password)
     if(!basscheck) return res.status(400).send({error:'invalid user or password'})
     const token=jwt.sign({email:user.email,id:user._id},process.env.secrate_key,{expiresIn:"1h"})
-  res.status(200).send({token})
+  res.status(200).send({ accessToken:token,login:true})
    } catch (error) {
     res.status(400).send(error.message)
    }
