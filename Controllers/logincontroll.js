16493d2a76912f8afc2e8c530b62b073b6ae91e7
hayaat <khadrabashiir4323@gmail.com>
2023-08-13 15:54:04 +0300

    antherlogin

--- Controllers/logincontroll.js
+++ Controllers/logincontroll.js
@@ -6,6 +6,11 @@ const express = require('express')
 require('dotenv').config();
 const jwt=require('jsonwebtoken')
   const loginpost =async(req,res)=>{
+   return res.send({
+    login:true,
+    body:req.body
+   })
+
     const {error}=loginvalidation(req.body)
     if(error) return res.status(400).send({error})
    try {
