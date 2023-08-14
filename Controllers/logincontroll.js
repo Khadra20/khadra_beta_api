629e31c79ec581dd0d6b9008ac4866698cfcb852
hayaat <khadrabashiir4323@gmail.com>
2023-08-14 07:52:57 +0300

    anatherone

--- Controllers/logincontroll.js
+++ Controllers/logincontroll.js
@@ -1,23 +1,56 @@
 
-const usermodel= require ('../Schema/userschema')
-const loginvalidation =require('../validation/loginval')
-const bycrypt =require('bcrypt')
-const express = require('express')
+const  usersModel = require('../Schema/userschema')
+const  loginvalidation  = require("../validation/loginval")
+const bcrypt=require('bcrypt')
+let jwt = require('jsonwebtoken');
 require('dotenv').config();
-const jwt=require('jsonwebtoken')
-  const loginpost =async(req,res)=>{
 
-    const {error}=loginvalidation(req.body)
-    if(error) return res.status(400).send({error})
-   try {
-    const user=await usermodel.findOne({email:req.body.email,userStatus:'active'})
-    if(!user) return res.status(400).send({error:"email not found"})
-    const basscheck=await bycrypt.compare(req.body.password,user.password)
-    if(!basscheck) return res.status(400).send({error:'invalid user or password'})
-    const token=jwt.sign({email:user.email,id:user._id},process.env.secrate_key,{expiresIn:"1h"})
-  res.status(200).send({ accessToken:token,login:true})
-   } catch (error) {
-    res.status(400).send(error.message)
-   }
-}
+//login function
+const loginpost = async (req, res) => {
+    try {
+        // validation
+        const { error } = loginvalidation(req.body);
+        if (error) return res.status(449).send(error.message);
+    
+        // find user data
+        const usergetdata = await usersModel.findOne({
+            email: req.body.email,
+        });
+        if (!usergetdata)
+          return res.status(401).send({
+            status: false,
+            message: 'username or password is incorrect',
+          });
+    
+        // check password
+        const checkpass = await bcrypt.compare(
+          req.body.password,
+          usergetdata.password
+        );
+        if (!checkpass)
+          return res.status(401).send({
+            status:false,
+            message: 'username or password is incorrect',
+          });
+        // token using jwt
+        const token = jwt.sign(
+          {
+            id: usergetdata._id,
+            name: usergetdata.name,
+            role:usergetdata.role
+          },
+          process.env.secrate_key
+        );
+        
+    
+        res.status(200).header('token', token).json({
+          status: true,
+          message: 'successfully logged in',
+          token: token,
+        });
+      } catch (error) {
+        res.status(400).send(error.message);
+      }
+};
+
 module.exports = {loginpost};
